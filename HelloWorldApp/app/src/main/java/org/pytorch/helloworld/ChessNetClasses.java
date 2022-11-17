b23c7f46d37232bb1b8aaaf41ea5d0ed9839a936
Pai Peng <sipaipv6@gmail.com>
2022-11-17 11:37:51 +0800

    add chess piece image and classes

---
+++ HelloWorldApp/app/src/main/java/org/pytorch/helloworld/ChessNetClasses.java
@@ -0,0 +1,18 @@
+package org.pytorch.helloworld;
+
+public class ChessNetClasses {
+    public static String[] IMAGENET_CLASSES = new String[]{
+            "B_BISHOP, Black Bishop",
+            "B_KING, Black King",
+            "B_KNIGHT, Black Knight",
+            "B_PAWN, Black Pawn",
+            "B_QUEEN, Black Queen",
+            "B_ROOK, Black Rook",
+            "W_BISHOP, White Bishop",
+            "W_KING, White King",
+            "W_KNIGHT, White Knight",
+            "W_PAWN, White Pawn",
+            "W_QUEEN, White Queen",
+            "w_ROOK, White Rook"
+    };
+}
